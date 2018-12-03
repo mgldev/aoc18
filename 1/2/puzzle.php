c4ae2d66d6550b01f8d662f7428b7a7554eb03f9
Michael Leach <michael.leach@purplewifi.com>
2018-12-03 10:48:54 +0000

    Completes 1.2

--- 1/2/puzzle.php
+++ 1/2/puzzle.php
@@ -1,4 +1,25 @@
 <?php
 
 // day one, puzzle two
+$frequencyModifiers = explode(PHP_EOL, file_get_contents(__DIR__ . '/../input.txt'));
 
+$repeatFound = false;
+$frequencies = [];
+$currentFrequency = 0;
+
+while (!$repeatFound) {
+    foreach ($frequencyModifiers as $frequencyModifier) {
+        $currentFrequency += $frequencyModifier;
+        if (in_array($currentFrequency, $frequencies)) {
+            $repeatFound = true;
+            break;
+        }
+        $frequencies[] = $currentFrequency;
+    }
+}
+
+if ($repeatFound) {
+    echo 'First repeating frequency is ' . $currentFrequency;
+} else {
+    echo 'No repeat found';
+}
