9b1f45a68ea1d4ac074f97fb38da755b4ba7f837
Michael Leach <michaelgeorgeleach@gmail.com>
2018-12-07 14:43:52 +0000

    Completes 3.1

--- 3/1/puzzle.php
+++ 3/1/puzzle.php
@@ -1,68 +1,23 @@
 <?php
 
-/**
- * Class Claim
- */
-class Claim
-{
-    /** @var string */
-    private $id;
+require_once 'Claim.php';
 
-    /** @var int */
-    private $top;
+/** @var Claim[] $claims */
+$claims = array_map(function(string $claimString) {
+    return Claim::fromString($claimString);
+}, explode(PHP_EOL, file_get_contents(__DIR__ . '/../input.txt')));
 
-    /** @var int */
-    private $left;
+$overlapping = 0;
+$map = [];
 
-    /** @var int */
-    private $width;
-
-    /** @var int */
-    private $height;
-
-    /**
-     * Claim constructor.
-     *
-     * @param string $id
-     * @param int $top
-     * @param int $left
-     * @param int $width
-     * @param int $height
-     */
-    public function __construct(string $id, int $top, int $left, int $width, int $height)
-    {
-        $this->id = $id;
-        $this->top = $top;
-        $this->left = $left;
-        $this->width = $width;
-        $this->height = $height;
-    }
-
-    /**
-     * @param string $claimString
-     *
-     * @return Claim
-     */
-    public static function fromString(string $claimString): self
-    {
-        $matches = [];
-        preg_match('/#(\d{3}).*(\d+),(\d+).*(\d+)x(\d+)/', $claimString, $matches);
-
-        return new self($matches[1], $matches[2], $matches[3], $matches[4], $matches[5]);
-    }
-
-    /**
-     * Determine if this claim overlaps another claim
-     *
-     * @param Claim $claim
-     *
-     * @return bool
-     */
-    public function overlaps(Claim $claim): bool
-    {
-
+foreach ($claims as $claim) {
+    foreach ($claim->getClaimGrid() as $occupiedCoordinate) {
+        if (!isset($map[$occupiedCoordinate])) {
+            $map[$occupiedCoordinate] = [];
+        }
+        $map[$occupiedCoordinate][] = $claim->getId();
+        $overlapping += (int) count($map[$occupiedCoordinate]) === 2;
     }
 }
 
-$claim = Claim::fromString('#123 @ 3,2: 5x4');
-die(var_dump($claim));
+die((string) $overlapping);
