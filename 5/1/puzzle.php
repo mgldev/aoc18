6b134992c996c0a705307abf55e16a34f9cd67b3
Michael Leach <michael.leach@purplewifi.com>
2018-12-13 16:36:03 +0000

    Simplifies logic

--- 5/1/puzzle.php
+++ 5/1/puzzle.php
@@ -3,18 +3,14 @@
 $chars = str_split(file_get_contents(__DIR__ . '/../input.txt'));
 $i = 0;
 
-while ($i < count($chars)) {
+while (isset($chars[$i + 1])) {
     $currentChar = $chars[$i];
-    if (!isset($chars[$i + 1])) {
-        break;
-    }
     $nextChar = $chars[$i + 1];
     $destroy = (($a = ord($currentChar) - ord($nextChar)) < 0 ? $a * -1 : $a) === 32;
     if ($destroy) {
         unset($chars[$i], $chars[$i + 1]);
         $chars = array_values($chars);
-        $i -= 2;
-        if ($i < 0) $i = 0;
+        $i = ($i - 2 < 0 ? 0 : $i - 2);
         continue;
     }
     $i++;
