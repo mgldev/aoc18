30af331bdb502d1248d806342b90b6edfc05a056
Michael Leach <michael.leach@purplewifi.com>
2018-12-21 09:18:48 +0000

    Simplifies instantiation of object

--- 6/1/puzzle.php
+++ 6/1/puzzle.php
@@ -7,4 +7,4 @@ require_once __DIR__ . '/Classes/CompletedStepVisitor.php';
 require_once __DIR__ . '/Classes/DependencyResolver.php';
 
 $stepCollection = (new StepCollectionBuilder())->build(__DIR__ . '/../input.txt');
-echo (new DependencyResolver())->resolve($stepCollection);
+echo (new DependencyResolver)->resolve($stepCollection);
